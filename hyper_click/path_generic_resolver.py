4007a8c9f6d4701d62c8eac331ae5181f9dbb837
Cristian Lorsson <cristian@cristian.im>
2017-11-22 16:08:35 -0200

    Refactor the generic resolver
    - Replaces the resolvers for Less, PHP and HTML with a new generic resolver.
    - Removes the old generic resolver.
    - Adds support for CSS, Pug, SugarSS, SugarML, Nunjucks, Jinja2, Twig, all using the new generic resolver.

--- hyper_click/path_generic_resolver.py
+++ hyper_click/path_generic_resolver.py
@@ -3,7 +3,7 @@ from os import path
 
 
 # A default resolver that resolves to a subfolder along along with one of the valid extensions
-class PhpPathResolver:
+class GenericPathResolver:
     def __init__(self, str_path, current_dir, roots, lang, settings):
         self.str_path = str_path
         self.current_dir = current_dir
@@ -15,9 +15,16 @@ class PhpPathResolver:
     def resolve(self):
         combined = path.realpath(path.join(self.current_dir, self.str_path))
 
+        # Try to match the literal filename referenced
+        # match: '../variables/palette.less'
+        if path.isfile(combined):
+            return combined
+
         # Loop all allowed extensions, and try matching those
+        # match '../variables/palette' to '../variables/palette.less'
         for ext in self.valid_extensions:
-            file_path = combined
+            file_path = combined + '.' + ext
             if path.isfile(file_path):
                 return file_path
         return ''
+
--- hyper_click/path_resolver.py
+++ hyper_click/path_resolver.py
@@ -2,11 +2,8 @@
 from os import path
 from .js_path_resolver import JsPathResolver
 from .sass_path_resolver import SassPathResolver
-from .less_path_resolver import LessPathResolver
 from .jstl_path_resolver import JstlPathResolver
-from .php_path_resolver import PhpPathResolver
-from .html_path_resolver import HTMLPathResolver
-from .path_generic_subfolder_resolver import GenericSubfolderResolver
+from .path_generic_resolver import GenericPathResolver
 
 
 class HyperClickPathResolver:
@@ -17,16 +14,10 @@ class HyperClickPathResolver:
             self.resolver = JsPathResolver(str_path, current_dir, roots, lang, settings)
         elif lang == 'sass':
             self.resolver = SassPathResolver(str_path, current_dir, roots, lang, settings)
-        elif lang == 'less':
-            self.resolver = LessPathResolver(str_path, current_dir, roots, lang, settings)
-        elif lang == 'php':
-            self.resolver = PhpPathResolver(str_path, current_dir, roots, lang, settings)
-        elif lang == 'html':
-            self.resolver = HTMLPathResolver(str_path, current_dir, roots, lang, settings)
         elif lang == 'jstl':
             self.resolver = JstlPathResolver(view, str_path, current_dir, roots, lang, settings)
         else:
-            self.resolver = GenericSubfolderResolver(str_path, current_dir, roots, lang, settings)
+            self.resolver = GenericPathResolver(str_path, current_dir, roots, lang, settings)
 
     def resolve(self):
         return self.resolver.resolve()
