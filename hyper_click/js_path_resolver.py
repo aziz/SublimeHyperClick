5676c3b01c977d20661690f5e86c5599a7b614e5
Ivan Nikolić <niksy5@gmail.com>
2019-04-30 11:25:06 +0200

    Add support for per-project JS lookup paths

--- hyper_click/js_path_resolver.py
+++ hyper_click/js_path_resolver.py
@@ -44,9 +44,11 @@ class JsPathResolver:
         self.settings = settings
         self.roots = roots
         self.valid_extensions = settings.get('valid_extensions', {})[lang]
+        self.proj_settings = proj_settings
         self.vendor_dirs = settings.get('vendor_dirs', {})[lang];
         self.matchingRoot = [root for root in self.roots if self.current_dir.startswith(root)]
         self.currentRoot = self.matchingRoot[0]
+        self.lookup_paths = self.proj_settings.get('lookup_paths', {}).get(lang, False) or settings.get('lookup_paths', {}).get(lang, False) or []
 
     def resolve(self):
         if find_index(NODE_CORE_MODULES, self.str_path) != -1:
@@ -81,12 +83,10 @@ class JsPathResolver:
                 return result
 
     def resolve_in_lookup_paths(self, target):
-        lookup_paths = self.settings.get('lookup_paths', {})[self.lang] or []
-        for lookup_path in lookup_paths:
+        for lookup_path in self.lookup_paths:
             result = self.resolve_relative_to_dir(target, path.join(self.currentRoot, lookup_path))
             if result:
                 return result
-
 
     def resolve_as_file(self, path_name):
         if path.isfile(path_name):
