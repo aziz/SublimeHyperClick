29005cc999d773922782fa2dcf5c7a32b15cf7b8
Richard Ayotte <rich@ayottesoftware.com>
2019-11-17 21:04:46 -0500

    Add support for aliases

--- hyper_click/js_path_resolver.py
+++ hyper_click/js_path_resolver.py
@@ -46,11 +46,17 @@ class JsPathResolver:
         self.valid_extensions = settings.get('valid_extensions', {})[lang]
         self.proj_settings = proj_settings
         self.vendor_dirs = settings.get('vendor_dirs', {})[lang];
+        self.aliases = settings.get('aliases',{})[lang]
         self.matchingRoots = [root for root in self.roots if self.current_dir.startswith(root)]
         self.currentRoot = self.matchingRoots[0] if self.matchingRoots else self.current_dir
         self.lookup_paths = self.proj_settings.get('lookup_paths', {}).get(lang, False) or settings.get('lookup_paths', {}).get(lang, False) or []
 
     def resolve(self):
+        for alias, alias_source in self.aliases.items():
+            result = self.resolve_from_alias(alias, alias_source)
+            if result:
+                return result
+
         if find_index(NODE_CORE_MODULES, self.str_path) != -1:
             return NODE_CORE_MODULES_TEMPLATE.format(self.str_path)
 
@@ -66,7 +72,13 @@ class JsPathResolver:
         result = self.resolve_in_lookup_paths(self.str_path)
         if result:
             return result
+
         return self.resolve_node_modules(self.str_path, self.current_dir)
+    def resolve_from_alias (self, alias, alias_source):
+        alias_path = path.join(alias_source, self.str_path[len(alias) + 1:])
+        result = self.resolve_relative_to_dir(alias_path, self.currentRoot)
+        if result:
+            return result
 
     def resolve_relative_to_dir(self, target, directory):
         combined = path.realpath(path.join(directory, target))
