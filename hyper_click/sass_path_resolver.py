8f1c5e182fb029a44f1eec22e3a4723834d796b4
Cristian Lorsson <cristian@cristian.im>
2020-01-06 21:59:26 -0300

    Add aliases support to Sass resolver

--- hyper_click/sass_path_resolver.py
+++ hyper_click/sass_path_resolver.py
@@ -11,11 +11,22 @@ class SassPathResolver:
         self.roots = roots
         self.valid_extensions = settings.get('valid_extensions', {})[lang]
         self.proj_settings = proj_settings
+
+        self.aliases = settings.get('aliases', {}).get(lang, {})
+        self.proj_aliases = proj_settings.get('aliases', {}).get(lang, {})
+        for alias in self.proj_aliases:
+            self.aliases[alias] = self.proj_aliases[alias]
+
         self.matchingRoots = [root for root in self.roots if self.current_dir.startswith(root)]
         self.currentRoot = self.matchingRoots[0] if self.matchingRoots else self.current_dir
         self.lookup_paths = self.proj_settings.get('lookup_paths', {}).get(lang, False) or settings.get('lookup_paths', {}).get(lang, False) or []
 
     def resolve(self):
+        for alias, alias_source in self.aliases.items():
+            result = self.resolve_from_alias(alias, alias_source)
+            if result:
+                return result
+
         result = self.resolve_relative_to_dir(self.str_path, self.current_dir)
         if result:
             return result
@@ -25,6 +36,12 @@ class SassPathResolver:
             return result
 
         return ''
+
+    def resolve_from_alias(self, alias, alias_source):
+        alias_path = path.join(alias_source, self.str_path[len(alias) + 1:])
+        result = self.resolve_relative_to_dir(alias_path, self.currentRoot)
+        if result:
+            return result
 
     def resolve_relative_to_dir(self, target, directory):
         combined = path.realpath(path.join(directory, target))
