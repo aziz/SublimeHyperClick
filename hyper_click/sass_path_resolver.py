394c4ef2ec51ae77949925c4119c41bc0b09693e
Koen Lageveen <koen.lageveen@me.com>
2020-07-01 20:30:02 +0200

    support sass imports with extension

--- hyper_click/sass_path_resolver.py
+++ hyper_click/sass_path_resolver.py
@@ -49,10 +49,11 @@ class SassPathResolver:
                 return result
 
     def resolve_as_file(self, path_name):
+        if path.isfile(path_name):
+            return path_name
         # matching ../variables/palette to ../variables/palette.scss
-        combined = path_name
         for ext in self.valid_extensions:
-            file_path = combined + '.' + ext
+            file_path = path_name + '.' + ext
             if path.isfile(file_path):
                 return file_path
 
